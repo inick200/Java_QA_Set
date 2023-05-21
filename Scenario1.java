c75960d3297e9343a44b5971230695b7929dc15c
Abhishek G <inick200@icloud.com>
2023-05-21 12:34:31 +0530

    added java based scenario Q&A's

---
+++ Scenario1.java
@@ -0,0 +1,25 @@
+package Java_QA_Set;
+// This series is one of the set from scenario based java questions. Guess the output ?
+public class Scenario1 {
+    static void test() throws RuntimeException {
+        try {
+            System.out.println("test");
+            throw new RuntimeException();
+        } catch (Exception e) {
+            System.out.println("exception");
+        }
+    }
+    public static void main(String[] args) {
+        try {
+            test();
+        } catch (Exception e) {
+            System.out.println("runtime ex");
+        }
+        System.out.println("end");
+    }
+}
+
+// O/P ---> 
+        // test
+        // exception
+        // end
