c4a78af5b652912b879afa3cb1b0f1990b9a4079
Abhishek G <inick200@icloud.com>
2023-05-27 13:08:39 +0530

    Added Java QA Scenario 12

---
+++ Scenario12.java
@@ -0,0 +1,29 @@
+package Java_QA_Set;
+
+/** @author Abhishek KG. */
+public class Scenario12 {
+    int val = 85;
+    Scenario12(){
+        val = 41;
+    }
+    {
+        val = 27;
+    }
+    public static void main(String[] args) {
+        Scenario12 obj = new Scenario12();
+        System.out.println(obj.val);
+    }
+}
+
+
+// OPTIONS: 
+//          a) 27
+//          b) 85
+//          c) 41
+//          d) 0
+
+
+
+
+// OUTPUT:
+//          c) 41 (CORRECT OPTION) ✓
