9ed80a82229ad8cb6c45a2956e97e13521cc1f68
Abhishek G <inick200@icloud.com>
2023-05-28 17:28:43 +0530

    Added Java QA Scenario 13 Polymorphism

---
+++ Scenario13.java
@@ -0,0 +1,40 @@
+package Java_QA_Set;
+
+class P {
+    P(){
+        System.out.println("super call");
+    }
+    public String m(String s){
+        return "parent " +s;
+    }
+}
+class C extends P {
+    C(){
+        System.out.println("C contructor");
+    }
+    public String m(String s){
+        return "child " +s;
+    }
+}
+
+public class Scenario13 extends C {
+    public static void main(String[] args) {
+        P obj = new C();
+        System.out.println(obj.m("class"));
+    }
+}
+
+
+// Identify the correct order of execution ?
+
+// OPTIONS:
+//          1.  C contructor child class
+//          2.  super call C contructor child class
+//          3.  child class super call parent class
+//          4.  C contructor parent class
+//          5.  Compilation error
+
+
+
+// OUTPUT: 
+//          2. super call C contructor child class (CORRECT OPTION) ✓
